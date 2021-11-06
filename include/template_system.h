f1131a1e38ffd81e3d18f46bd7f427f3674aef1c
ravi688 <rp0412204@gmail.com>
2021-11-06 20:22:02 +0530

    Updated header file

--- include/template_system.h
+++ include/template_system.h
@@ -1,4 +1,18 @@
-
 #pragma once
 
 #define template(typename, T) typename##_##T
+
+
+#define template1(typename, T) template(typename, T)
+#define template2(typename, T1, T2) typename##_##T1##T2
+
+
+typedef char* pchar_t;
+typedef int* pint_t;
+typedef float* pfloat_t;
+typedef double* pdouble_t;
+
+typedef const char* pcchar_t;
+typedef const int* pcint_t; 
+typedef const float* pcfloat_t;
+typedef const double* pcdouble_t;
